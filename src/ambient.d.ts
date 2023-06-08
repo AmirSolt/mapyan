15c45d675467651448989919ab9fee72c58ffc27
AmirSolt <soltamir194@gmail.com>
2023-06-08 13:38:18 -0400

    starting again

---
+++ src/ambient.d.ts
@@ -0,0 +1,23 @@
+
+
+// ====== generate type ???
+// interface SearchProduct{
+// 	asin:string;
+// 	country:string;
+// 	title:string;
+// 	image:string;
+// 	brand:string;
+// 	link:string;
+// 	rating:number;
+// 	price:Price;
+// }
+// interface ProductInfo{
+// 	asin:string;
+// 	description:string;
+// }
+
+// interface Price{
+// 	symbol:string;
+// 	value:number;
+// 	currency:string;
+// }
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import Header from '$lib/comp/general/Header.svelte';
+	import Footer from '$lib/comp/general/Footer.svelte';
+	
+	
+	// import { onMount } from 'svelte';
+	// import { loadUserCountry } from '$lib/utils/country';
+	// onMount(() => {
+	// 	loadUserCountry();
+	// });
+	
+	
+
+	// ============== Toasts =================
+	import { Toast } from '@skeletonlabs/skeleton';
+	
+</script>
+
+<svelte:head>
+	<title>MAPYAN</title>
+	<meta name="description" content="Pick any two amazon products and generate a comparison table using AI. Our innovative platform empowers you to generate comprehensive and insightful comparison tables, providing you with a side-by-side analysis of key features, specifications, customer reviews, and more.">
+	<meta name="keywords" content="Mapyan, Amazon product comparison, AI-powered comparison tables, Dynamic product comparisons, Smart shopping tool, Online shopping assistant, Amazon price comparison, Customer reviews analysis, Product feature comparison, Effortless product research,">
+	<link rel="icon" type="image/svg+xml" href="/logo.svg" />
+
+</svelte:head>
+
+
+
+<AppShell>
+
+
+	<svelte:fragment slot="pageHeader">
+		<header>
+			<Header />
+		</header>
+	</svelte:fragment>
+	
+	<Toast position='t' background='variant-filled-error' />
+	<div class="flex justify-center w-full h-full">
+		<div class="max-w-7xl p-2 md:p-4 w-full h-full">
+			<slot />
+		</div>
+	</div>
+
+	<svelte:fragment slot="pageFooter">
+		<footer>
+			<Footer />
+		</footer>
+	</svelte:fragment>
+
+
+</AppShell>
